4721ee72c61a45b5227b6d4bc741407936e0301e
mahadnifsey <mahadnifsey@gmail.com>
2022-05-30 23:46:31 +0100

    corrected winners outcome & new animations

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
+var vid1 = document.getElementById("video1");
+var vid2 = document.getElementById("video2");
+
+playVid1 = () => { vid1.play(); };
+playVid2 = () => { vid2.play(); };
+
+function clearResult() {
+  console.log(document.getElementById('uPick').innerHTML ="");
+  console.log(document.getElementById('main').innerHTML = "");
+  document.getElementById("video1").style.display = 'none';
+  document.getElementById("video2").style.display = 'none';
+};
+
 function storeVar(value){
-  // User Selection
-  let userChoice = value;
-  console.log(userChoice);
-    
-  // Computer Selection
-  let choiceStack = ['paper', 'rock', 'scissors'];
+// First Clear the field
+clearResult();
+// User Selection
+let userChoice = value;
+console.log(userChoice);
   
-  let randomNum = Math.floor(Math.random() * 3);
+// Computer Selection
+let choiceStack = ['paper', 'rock', 'scissors'];
+
+let randomNum = Math.floor(Math.random() * 3);
+
+let computerChoice = choiceStack[randomNum];
+
+console.log(document.getElementById('uPick').innerHTML = `Your choice is ${userChoice}, the computer's choice is ${computerChoice}.`);
   
-  let computerChoice = choiceStack[randomNum];
-  
-  console.log(document.getElementById('uPick').innerHTML = `Your choice is ${userChoice}, the computer's choice is ${computerChoice}.`);
-    
-  // Selecting a Winner based on selections
-  const determineWinner = (userChoice, computerChoice) => {
+// Selecting a Winner based on selections
+const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice){
     console.log(document.getElementById('main').innerHTML = "Tie!");
   }else if (
     userChoice === 'paper' && computerChoice === 'rock'){
     console.log(document.getElementById('main').innerHTML = "You win!");
+    document.getElementById("video1").style.display = 'block';
+    playVid1();
   }else if (userChoice === 'rock' && computerChoice === 'scissors'){
     console.log(document.getElementById('main').innerHTML = "You win!");
-  }else if (userChoice === 'scissors' && computerChoice === 'rock'){
-    console.log(document.getElementById('main').innerHTML = "You lose!");
-  }else if (userChoice === 'rock' && computerChoice === 'paper'){
-    console.log(document.getElementById('main').innerHTML = "You lose!");
-  }else if (userChoice === 'paper' && computerChoice === 'scissors'){
-    console.log(document.getElementById('main').innerHTML = "You lose!");
-  }else if (userChoice === 'scissors' || computerChoice === 'paper'){
-    console.log(document.getElementById('main').innerHTML = "You lose!");
+    document.getElementById("video1").style.display = 'block';
+    playVid1();
+  }else if (userChoice === 'scissors' && computerChoice === 'paper'){
+    console.log(document.getElementById('main').innerHTML = "You win!");
+    document.getElementById("video1").style.display = 'block';
+    playVid1();
   }else{
-    console.log(document.getElementById('main').innerHTML = "Invalid input, please try again");
+    console.log(document.getElementById('main').innerHTML = "You lose!");
+    document.getElementById("video2").style.display = 'block';
+    playVid2();
   };
 };
-  
-  console.log(determineWinner(userChoice, computerChoice));
 
-  };
-  
-  // Clearing everything.
-  function clearResult() {
-    console.log(document.getElementById('uPick').innerHTML ="");
-    console.log(document.getElementById('main').innerHTML = "");
-  }
+console.log(determineWinner(userChoice, computerChoice));
+
+};
